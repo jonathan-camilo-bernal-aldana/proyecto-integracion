a544904bf67597570bdc2f6efcf67e14071ec2bf
Jonathan Bernal <jonathan.bernal@insttantt.com>
2022-12-08 20:19:27 -0500

    add changes

--- ws/index.php
+++ ws/index.php
@@ -17,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         $sql = $pdo->prepare("SELECT * FROM libro");
         $sql->execute();
         $sql->setFetchMode(PDO::FETCH_ASSOC);
-        header("HTTP/1.1 200 OK asdf");
+        header("HTTP/1.1 200 OK asdfadf");
         echo json_encode($sql->fetchAll());
         exit;
     }
